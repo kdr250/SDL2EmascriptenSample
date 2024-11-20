5522ab4c853a6e90820362b55766e1b0e12c7c1e
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-11-20 17:04:55 +0900

    OpenGLを使うようにする

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,38 +1,35 @@
+#include <GL/glew.h>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
 #include <string>
 
 #ifdef __EMSCRIPTEN__
+    #include <SDL2/SDL_opengles2.h>
     #include <emscripten.h>
+    #include <emscripten/html5.h>
 #endif
-
-#undef main  //Needed for windows.
 
 bool running = true;
 SDL_Window* window;
-SDL_Renderer* renderer;
-SDL_Texture* texture;
+SDL_GLContext context;
 
-SDL_Texture* loadTexture(const std::string& file, SDL_Renderer* renderer)
+bool loadShaders()
 {
-    // Load from file
-    SDL_Surface* surface = IMG_Load(file.c_str());
-    if (!surface)
-    {
-        SDL_Log("Failed to load texture file %s", file.c_str());
-        return nullptr;
-    }
+    // TODO
+    return true;
+}
 
-    // Create texture from surface
-    SDL_Texture* result = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
-    if (!result)
-    {
-        SDL_Log("Failed to convert surface to texture for %s", file.c_str());
-        return nullptr;
-    }
+bool loadTexture(const std::string& file, unsigned int& textureId)
+{
+    // TODO
+    return true;
+}
 
-    return result;
+void terminate()
+{
+    // Terminate SDL
+    SDL_GL_DeleteContext(context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
 
 void mainloop()
@@ -40,12 +37,9 @@ void mainloop()
     if (!running)
     {
 #ifdef __EMSCRIPTEN__
-        // Terminate SDL
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        IMG_Quit();
-        SDL_Quit();
         emscripten_cancel_main_loop(); /* this should "kill" the app. */
+        terminate();
+        return;
 #endif
     }
 
@@ -65,24 +59,11 @@ void mainloop()
         running = false;
     }
 
-    // set draw color blue
-    SDL_SetRenderDrawColor(renderer,
-                           0,    // R
-                           0,    // G
-                           255,  // B
-                           255   // A
-    );
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);  // set the clear color to blue
+    glClear(GL_COLOR_BUFFER_BIT);          // Clear the color buffer
 
-    // clear back buffer
-    SDL_RenderClear(renderer);
-
-    // Draw texture
-    int x = SDL_GetTicks() % 1024;
-    SDL_Rect rect {x, 100, 100, 100};
-    SDL_RenderCopy(renderer, texture, nullptr, &rect);
-
-    // exchange front buffer for back buffer
-    SDL_RenderPresent(renderer);
+    // swap the buffers
+    SDL_GL_SwapWindow(window);
 }
 
 int main(int argc, char* argv[])
@@ -94,13 +75,30 @@ int main(int argc, char* argv[])
         return EXIT_FAILURE;
     }
 
+// Set OpenGL attributes
+#ifdef __EMSCRIPTEN__
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+#else
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+                        SDL_GL_CONTEXT_PROFILE_CORE);      // Use the core OpenGL profile
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);  // Specify version 3.3
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+#endif
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);  // Request a color buffer with 8-bits per RGBA channel
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);        // Enable double buffering
+    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);  // Force OpenGL to use hardware acceleration
+
     window = SDL_CreateWindow("Hello world !!",         // window title
                               SDL_WINDOWPOS_UNDEFINED,  // Top left x-coordinate of window
                               SDL_WINDOWPOS_UNDEFINED,  // Top left y-coordinate of window
                               1024,                     // width of window
                               768,                      // height of window
-                              0                         // flags (0 for no flags set)
-    );
+                              SDL_WINDOW_OPENGL);
 
     if (!window)
     {
@@ -108,23 +106,18 @@ int main(int argc, char* argv[])
         return EXIT_FAILURE;
     }
 
-    renderer = SDL_CreateRenderer(window,  // window to create renderer for
-                                  -1,      // usually -1
-                                  0);
+    // Create an OpenGL context
+    context = SDL_GL_CreateContext(window);
 
-    if (!renderer)
+    // Initialize GLEW
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
     {
-        SDL_Log("Failed to create renderer: %s", SDL_GetError());
+        SDL_Log("Failed to initialize GLEW.");
         return EXIT_FAILURE;
     }
 
-    if (IMG_Init(IMG_INIT_PNG) == 0)
-    {
-        SDL_Log("Unable to initialize SDL_image: %s", SDL_GetError());
-        return EXIT_FAILURE;
-    }
-
-    texture = loadTexture("resources/example.png", renderer);
+    glGetError();  // On some platforms, GLEW will emit a benign error code, so clear it
 
     // Main Loop
 #ifdef __EMSCRIPTEN__
@@ -134,12 +127,7 @@ int main(int argc, char* argv[])
     {
         mainloop();
     }
-
-    // Terminate SDL
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    IMG_Quit();
-    SDL_Quit();
+    terminate();
 #endif
 
     return EXIT_SUCCESS;
