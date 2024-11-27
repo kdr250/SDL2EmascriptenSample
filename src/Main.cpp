9c7eafbe56b93bc0925c27b0f8e036656be8fa19
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-11-27 16:28:10 +0900

    wasm化した際に文字が表示されなかったので修正

--- src/Main.cpp
+++ src/Main.cpp
@@ -447,6 +447,9 @@ void initializeFont()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
     fontTextureWidth  = texture->w;
     fontTextureHeight = texture->h;
 
